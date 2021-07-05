1b4aabbc4edc4fe80dab48b094a68206c72f437f
datadraconis <71951112+datadraconis@users.noreply.github.com>
2021-07-05 17:04:23 -0400

    Create StegoGen.sh

---
+++ StegoGen.sh
@@ -0,0 +1,23 @@
+!/bin/bash
+
+# StegGen
+# Description: EMH script to generate stego files for CTFs
+# Author: datadraconis
+
+# Generate flag content
+TEXT=$(python3 flagGen.py 3 words.txt)
+
+FLAG="{$TEXT}"
+
+# Generate flag text file
+echo "$FLAG" > tmp.tmp
+
+# Generate stego image
+
+cat image.jpeg tmp.tmp > stego.jpeg
+
+# Generate clue file
+echo "$FLAG" > stego.clu
+
+# Cleanup
+rm tmp.tmp
